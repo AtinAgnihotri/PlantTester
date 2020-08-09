18a5d3822ffc97fd74f95c33a8e1bba18d8d040d
Atin Agnihotri <atinagnihotri@gmail.com>
2020-08-10 01:56:55 +0530

    WIP EmailReports : HTMLBuilder Done

--- EmailReports/CreateReport/HTMLBuilder.py
+++ EmailReports/CreateReport/HTMLBuilder.py
@@ -1,4 +1,6 @@
-#TODO
+'''
+Builds the HTML String block for the Multipart Email Message
+'''
 class HTMLBuilder:
     # region Initialisation
     def __init__(self):
@@ -13,11 +15,17 @@ class HTMLBuilder:
     # region Helper Methods
     def __buildBasicHtmlStructure(self):
         '''
-
-        :return:
+        Builds the HTML text blcok for Multipart message of email
+        :return: HTML String
         '''
         html = '<html>\n'
         html += '<body>\n'
+        html += '<style>\n'
+        html += 'table, th, td {\n'
+        html += '  border: 1px solid black;\n'
+        html += '  border-collapse: collapse;\n'
+        html += '}\n'
+        html += '</style>\n'
         html += '{TITLE_OF_REPORT}\n'
         html += '{CUMULATIVE_RESULTS_TABLE}\n'
         html += '{INFO_ON_REPORT}\n'
@@ -26,55 +34,110 @@ class HTMLBuilder:
         html += '</html>\n'
         return html
 
-    # TODO
-    def __buildReportTitle(self, buildSlug):
+    def __buildReportTitle(self):
         '''
-
-        :param buildSlug:
-        :return:
+        Builds the HTML block for the report title
+        :return: HTML String for Title
         '''
-        return ''
+        html = '<h1>\n'
+        html += '<font style="color:blue>\n'
+        html += '<center>\n'
+        html += 'PlantTester : App Test Results\n'
+        html += '</center>\n'
+        html += '</font>\n'
+        html += '</h1>\n'
+        return html
 
-    # TODO
     def __buildReportInfo(self, buildSlug, currentTime):
         '''
-
-        :param buildSlug:
-        :param currentTime:
-        :return:
+        Builds the HTML block for the Report Information
+        :param buildSlug: Bitrise Build Slug
+        :param currentTime: String of current date time
+        :return: HTML String for Report Information
         '''
-        return ''
+        html = '<br>\n'
+        html += '<p>\n'
+        html += 'PlantTester UI test results for the build ' + buildSlug + ' at ' + currentTime + '. \n'
+        html += 'You can find more about the build '
+        html += '<a href="' + self.__buildUrlPattern.replace('{BUILD_SLUG}', buildSlug)+ '">here</a>,\n'
+        html += 'and you can see the test results in detail '
+        html += '<a href="' + self.__testReportUrlPattern.replace('{BUILD_SLUG}', buildSlug)+ '">here</a>.\n'
+        html += '</p>\n'
+        html += '<br>\n'
+        return html
 
-    # TODO
     def __buildCumulativeResultsTable(self, resultsDict):
         '''
-
-        :param resultsDict:
-        :return:
+        Builds the HTML block for the Cumulative Results Table
+        :param resultsDict: Dictionary of Cumulative Results
+        :return: HTML String for Cumulative Results table
         '''
-        return ''
+        html = '<br>\n'
+        html += '<center>\n'
+        html += '<table style="width:25%>"\n'
+        html += '<th colspan="2"><bold>Test Results</bold><th>\n'
+        for eachKey in resultsDict.keys():
+            eachItem = str(eachKey)
+            eachValue = str(resultsDict[eachKey])
+            html += '<tr>\n'
+            if eachItem in ('Errors', 'Failures', 'Skipped'):
+                html += '<font style="color:red">\n'
+            else:
+                html += '<font style="color:yellow">\n'
+            html += '<td>' + eachItem + '</td>\n'
+            html += '<td>' + eachValue + '</td>\n'
+            html += '</font>\n'
+            html += '</tr>\n'
+        html += '</center>\n'
+        html += '</table>"\n'
+        return html
 
-    # TODO
     def __buildTestTables(self, logCatDict):
         '''
-
-        :param logCatDict:
+        Builds the HTML Block for tables of individual test cases and their steps
+        :param logCatDict: Logcat Dictionary of test cases and their steps
         :return:
         '''
-        return ''
+        html = '<br>\n'
+        html += '<h2>\n'
+        html += '<center>\n'
+        html += 'Test Cases\n'
+        html += '</center>\n'
+        html += '</h2>\n'
+
+        for eachCase in logCatDict.keys():
+            eachCaseName = str(eachCase)
+            html += '<table style="width:50%>"\n'
+            html += '<th colspan="2"><bold>'+ eachCaseName +'</bold><th>\n'
+            i = 1
+            for eachStep in logCatDict[eachCase]:
+                eachStepStr = str(eachStep)
+                html += '<tr>\n'
+                if 'ERROR' in eachStepStr:
+                    html += '<font style="color:red">\n'
+                html += '<td>' + str(i) + '</td>\n'
+                html += '<td>' + eachStepStr + '</td>\n'
+                if 'ERROR' in eachStepStr:
+                    html += '</font>\n'
+                html += '</tr>\n'
+                i += 1
+            html += '</table>"\n'
+            html += '<br>"\n'
+
+        return html
     # endregion
 
     def buildHtmlBlock(self, buildSlug, currentTime, resultsDict, logCatDict):
         '''
-
-        :param buildSlug:
-        :param currentTime:
-        :param resultsDict:
-        :param logCatDict:
-        :return:
+        Builds the HTML block for the Multipart Email Message
+        :param buildSlug: Bitrise Build Slug
+        :param currentTime: String of current date time
+        :param resultsDict: Dictionary of cumulative results
+        :param logCatDict: Dictionary of LogCat test cases and their steps
+        :return: String for the HTML block
         '''
         htmlStr = self.__buildBasicHtmlStructure()
-        htmlStr = htmlStr.replace('{TITLE_OF_REPORT}', self.__buildReportTitle(buildSlug))
+        htmlStr = htmlStr.replace('{TITLE_OF_REPORT}', self.__buildReportTitle())
         htmlStr = htmlStr.replace('{CUMULATIVE_RESULTS_TABLE}', self.__buildCumulativeResultsTable(resultsDict))
         htmlStr = htmlStr.replace('{INFO_ON_REPORT}', self.__buildReportInfo(buildSlug, currentTime))
         htmlStr = htmlStr.replace('{TEST_TABLES}', self.__buildTestTables(logCatDict))
